d7f124f68dcb325270ee2bad0b71d15c8081e7b7
Hridoy-shill <ridoyaaa60@gmail.com>
2023-03-11 11:28:53 +0600

    comment and new fuction added

--- savedata.js
+++ savedata.js
@@ -1,3 +1,4 @@
+/* DOM er maddome html er import field shomuher value gulo pawar jonno ai fuction */
 const addBtn = () =>{
     const productField = document.getElementById('products-feild');
     const productQuantity = document.getElementById('quantity-feild');
@@ -10,6 +11,7 @@ const addBtn = () =>{
     saveDataInLocalStorage(product, quantity);
 }
 
+/* store kora cart er data shomuho pawar jonno ai fuction*/
 const storeShoppingCart = () => {
     let cart = {};
     const storeCart = localStorage.getItem('cart');
@@ -19,6 +21,7 @@ const storeShoppingCart = () => {
     return cart;
 }
 
+/* store kora data gulo webpage a display korar jonno ai function */
 const displayProduct = (product, quantity) => {
     const displayContainer = document.getElementById('product-container');
     const li = document.createElement('li')
@@ -26,9 +29,23 @@ const displayProduct = (product, quantity) => {
     displayContainer.appendChild(li);
 }
 
+/* store cart fuction hote prapto data gulo ke stingify kore local storage a save korar jonno ai fuction */
 const saveDataInLocalStorage = (product, quantity) =>{
     const cart = storeShoppingCart();
     cart[product] = quantity;
     const cartStringified = JSON.stringify(cart);
     localStorage.setItem('cart', cartStringified)
 }
+
+/* local storage a save kora data gulo ke patmanent vabe display te ba web UI a show korar jonno ai fuction */
+const displayProductFromLocalStorage = () =>{
+    const saveCart = storeShoppingCart();
+    console.log(saveCart);
+    for(const product in saveCart){
+        const quantity = saveCart[product];
+        console.log(product, quantity);
+        displayProduct(product, quantity);
+    }
+}
+
+displayProductFromLocalStorage();
